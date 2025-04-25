b45d3d6b63cd6bad6381e404b5adfde717f27a70
ya0201 <23146443+ya0201@users.noreply.github.com>
2025-04-26 01:06:09 +0900

    use ipnet crate

--- src/commands/subnet/info.rs
+++ src/commands/subnet/info.rs
@@ -1,3 +1,9 @@
+extern crate ipnet;
+use ipnet::IpNet;
+use std::str::FromStr;
 pub fn run(cidr: String) {
-    println!("Subnet info: {}", cidr);
+    // println!("Subnet info: {}", cidr);
+    let net = IpNet::from_str(&cidr).unwrap();
+    println!("{} hostmask = {}", net, net.hostmask());
+    println!("{} netmask = {}", net, net.netmask());
 }
